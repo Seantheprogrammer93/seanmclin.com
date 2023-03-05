ab9abdca74ededddb5fb8940c5544d9389de6f72
Sean McLin <srmclin93@yahoo.com>
2023-03-04 21:23:23 -0800

    Commit on master

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -5,10 +5,4 @@ export default [
         stack: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
         link: 'https://demohsc.glitch.me/',
     },
-    {
-        title: 'C shell',
-        imgUrl: 'https://images.unsplash.com/photo-1629654297299-c8506221ca97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2148&q=80',
-        stack: ['C', 'CLI', 'Replit'],
-        link: 'https://replit.com/@Seantheprogramm/C-Shell#main.c',
-    },
 ]
