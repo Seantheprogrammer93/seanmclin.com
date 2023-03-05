d7a1fb2d60970d8b5e957f661c172b599e6af5c3
Sean McLin <srmclin93@yahoo.com>
2023-03-05 15:02:13 -0800

    Commit

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -6,7 +6,7 @@ export default [
         link: 'https://demohsc.glitch.me/',
     },
     {
-        title: 'Square It UP',
+        title: 'Square It Up',
         imgUrl: 'https://images.unsplash.com/photo-1635070041078-e363dbe005cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
         stack: ['C', 'Algorithms', 'Replit'],
         link: 'https://replit.com/@Seantheprogramm/C-Shell#main.c',
