f626feda3951d87bfbe478b378f74ddeb237f1c2
seantheprogrammer93 <srmclin93@yahoo.com>
2023-03-08 10:20:33 -0800

    Commit on master

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -6,9 +6,9 @@ export default [
         link: 'https://demohsc.glitch.me/',
     },
     {
-        title: 'Square It Up',
+        title: 'Fibonacci',
         imgUrl: 'https://images.unsplash.com/photo-1635070041078-e363dbe005cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
-        stack: ['C', 'Algorithms', 'Replit'],
-        link: 'https://replit.com/@Seantheprogramm/C-Shell#main.c',
+        stack: ['C/C++', 'Algorithms', 'Replit'],
+        link: 'https://replit.com/@Seantheprogramm/Fibonacci-Coding',
     },
 ]
