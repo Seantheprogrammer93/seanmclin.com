6525f4567cd3217c790584a5a84bfe70be4442c0
seantheprogrammer93 <srmclin93@yahoo.com>
2023-03-09 14:44:32 -0800

    Commit on master

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,11 +2,11 @@ export default [
     {
         title: 'Headless Shopping Cart',
         imgUrl: 'https://images.unsplash.com/photo-1557821552-17105176677c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3264&q=80',
-        stack: ['HTML', 'CSS', 'JavaScript', 'jQuery'],
+        stack: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'jsDeliver'],
         link: 'https://demohsc.glitch.me/',
     },
     {
-        title: 'Hosted OpenVPN',
+        title: 'Hosted OpenVPN Access Server',
         imgUrl: 'https://images.unsplash.com/photo-1544197150-b99a580bb7a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
         stack: ['VPN', 'Networking', 'Digital Ocean', 'OpenVPN'],
         link: 'https://openvpn.seanmclin.xyz/',
