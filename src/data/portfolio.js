27661dfe2ba1e2814e25bfe3a3b520d9b46de133
seantheprogrammer93 <srmclin93@yahoo.com>
2023-03-09 14:29:02 -0800

    Commit on master

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -6,6 +6,12 @@ export default [
         link: 'https://demohsc.glitch.me/',
     },
     {
+        title: 'Hosted Open Server',
+        imgUrl: 'https://images.unsplash.com/photo-1544197150-b99a580bb7a8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80',
+        stack: ['VPN', 'Networking', 'Digital Ocean', 'OpenVPN'],
+        link: 'https://openvpn.seanmclin.xyz/',
+    },
+    {
         title: 'Fibonacci',
         imgUrl: 'https://images.unsplash.com/photo-1635070041078-e363dbe005cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
         stack: ['C/C++', 'Algorithms', 'Replit'],
